138dcbfbe23604087f51d1ad4a443e2dfcb34d46
SoLoRi <SoLoRi@DESKTOP-545RIUK>
2022-06-18 03:35:18 +0700

    petCard mapping

--- src/components/pet/PetContainer.jsx
+++ src/components/pet/PetContainer.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import PetCard from "./PetCard";
 
 function PetContainer() {
+  const [animal, setAnimal] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      await fetchUser(pathname);
+      setAnimal(res.data.animals);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="flex flex-nowrap w-[1300px] h-[1200px] bg-[#F8F8F8] justify-center ml-[20%]">
-        <PetCard />
+        {animal.map((el) => (
+          <PetCard key={el.id} animal={el} fetchData={fetchData} />
+        ))}
       </div>
     </>
   );
